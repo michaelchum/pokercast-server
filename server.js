72609813277c961d4e423de75c32f1509198f4f8
Michael Ho <michaelhochum@gmail.com>
2014-09-20 07:00:54 -0400

    fjdalfjs

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var express = require('express'),
 	poker = require('./lib/node-poker'),
 	table;
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); // NOT SAFE FOR PRODUCTION
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
 app.get('/', function(req, res){
 	
 	res.send('Hello World!!!');
